45da40d9c5d6399dac580ac701cddf104cf377bb
hjwnu <tlstjr01@gmail.com>
2024-06-07 13:02:10 +0900

    fix config

---
+++ src/main/java/org/nullcom/nullcomcore/NullcomCoreApplication.java
@@ -0,0 +1,13 @@
+package org.nullcom.nullcomcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullcomCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullcomCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nullcom/nullcomcore/NullcomCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nullcom.nullcomcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullcomCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
